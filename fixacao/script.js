04d1fbf9db7d39360503113cc2192cc760ed7294
João Neiva <jg.neiva@gmail.com>
2023-03-20 11:54:19 -0300

    exercicio resolvido

--- fixacao/script.js
+++ fixacao/script.js
@@ -1,1 +1,15 @@
 // Resolva o Exercício de Fixação!
+
+function nomeação (nome) {
+    console.log(`Olá, ${nome}`)
+}
+
+nomeação("joão")
+nomeação(prompt("insira seu nome"))
+nomeação("lucas")
+
+const myconst = function(nome) {
+    console.log(`Olá, ${nome}`)
+}
+
+myconst("pedro")
--- pratica-guiada/script.js
+++ pratica-guiada/script.js
@@ -1,1 +1,19 @@
-// Acompanhe a Prática Guiada e repita o exercício no Momento de Réplica!
+function calculo (numero) {
+    const verifica = numero%2==0
+    const soma = numero+10
+    const multiplica = numero * numero
+
+    return `O numero ${numero} é par? ${verifica}. Somado com 10, o resultado é ${soma}, e multiplicado por ele mesmo o resultado é ${multiplica}`
+}
+
+console.log(calculo(218))
+
+const calculo2 = function(numero) {
+    const verifica = numero%2==0
+    const soma = numero+10
+    const multiplica = numero * numero
+
+    return `O numero ${numero} é par? ${verifica}. Somado com 10, o resultado é ${soma}, e multiplicado por ele mesmo o resultado é ${multiplica}`
+}
+
+console.log(calculo(217))
